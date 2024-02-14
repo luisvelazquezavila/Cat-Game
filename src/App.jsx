3fdb5d7ea91e4fa382a5d6d5bbe235e86ed939ff
luis <luisvelazquezavila@gmail.com>
2024-02-13 21:44:15 -0600

    Crée los archivos base de react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+const turns = {
+  x: "x",
+  o: "o"
+}
+
+const board = Array(9).fill(null);
+
+function App() {
+  return (
+    <>
+      <h1>Cat Game</h1>
+    </>
+  )
+}
+
+export default App
